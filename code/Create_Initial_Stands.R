465a198070b3aae2e66d35bed2d10eb1ce21a895
ameztegui <ameztegui@gmail.com>
2018-01-24 16:07:33 +0100

    Create initial stand densities

---
+++ code/Create_Initial_Stands.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+
+
+load("./data/condes_pars.Rdata")
+
+initial_stand <- function (species, Dg, Martonne) {
+     pars <- condes_pars%>%
+            filter(Code==species)  %>% 
+             mutate(N = exp((C0 + C1 * log(Martonne) + 
+                                   (E0 + E1* Martonne)*log(Dg))))
+      return(pars$N)
+      
+}
+
+initial_stand("PISY", 25, 29)
+initial_stand("PISY", 25, 93)
+initial_stand("PIPI", 25, 16)
+initial_stand("PIPI", 25, 39)
+initial_stand("PIHA", 25, 10)
+initial_stand("PIHA", 25, 37)
+initial_stand("PINI", 25, 21)
+initial_stand("PINI", 25, 75)
+initial_stand("PIPN", 25, 18)
+initial_stand("PIPN", 25, 82)
